0e39036aa05bafdd4ad1344ac355905894e87d62
Siwakorn Thitisaksakul <clever_buffalo@hotmail.com>
2017-10-03 21:31:55 +0700

    :tada: start create react app with nothing on the page

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css'
+
+import React, { Component } from 'react'
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        test
+      </div>
+    )
+  }
+}
+
+export default App
