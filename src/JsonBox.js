677b65627352b4d3719caa4f9f618719c50a06e6
Siwakorn Thitisaksakul <clever_buffalo@hotmail.com>
2017-10-03 22:27:08 +0700

    :sparkles: allow disable textarea to work for JsonBox

--- src/JsonBox.js
+++ src/JsonBox.js
@@ -3,6 +3,7 @@ import './JsonBox.css'
 import React, { Component } from 'react'
 
 import PropTypes from 'prop-types'
+import cn from 'classnames'
 
 class JsonBox extends Component {
 
@@ -23,8 +24,9 @@ class JsonBox extends Component {
   renderTextArea = () => {
     return (
       <textarea
-        className='json-box__text-area'
+        className={cn('json-box__text-area', { '--disabled': !this.props.onChange })}
         onChange={this.props.onChange} 
+        disabled={!this.props.onChange}
       />
     )
   }
